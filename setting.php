ed1201636546c511527558611055259613baccd5
Fiki Alamsyah <fiki@pop-os.localdomain>
2021-02-28 20:37:08 +0700

    add code to setting.php

---
+++ setting.php
@@ -0,0 +1,35 @@
+               <main>
+                    <div class="container-fluid">
+                            <h1 class="mt-4">Setting</h1>
+                            <ol class="breadcrumb mb-4">
+                                <li class="breadcrumb-item active">Setting</li>
+                            </ol>
+                            <form action="">
+                                <div class="form-group row">
+                                    <label for="staticEmail" class="col-sm-2 col-form-label">Name</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" readonly="" class="form-control w-25 bg-light" id="staticEmail" value="Fiki alamsyah">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="staticEmail" class="col-sm-2 col-form-label">Username</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" readonly="" class="form-control w-25 bg-light" id="staticEmail" value="Fiki">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="staticEmail" class="col-sm-2 col-form-label">Password</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" readonly="" class="form-control w-25 bg-light" id="staticEmail" value="****">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="staticEmail" class="col-sm-2 col-form-label">Level</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" readonly="" class="form-control w-25 bg-light" id="staticEmail" value="Admin">
+                                    </div>
+                                </div>
+                                <button type="submit" class="btn btn-primary">Edit</button>
+                            </form>
+                    </div>
+               </main>
